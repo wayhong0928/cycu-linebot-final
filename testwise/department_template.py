7008df4d39aa9b5d7c47a25a8a5a27db4d2d8276
ling <ling920415@gmail.com>
2024-05-23 21:06:34 +0800

    新增面試建議的圖(但右半部科系對應錯誤)

--- testwise/department_template.py
+++ testwise/department_template.py
@@ -5,11 +5,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TemplateSendMessage, MessageTemplateAction, TextSendMessage, TextMessage, PostbackEvent, PostbackTemplateAction, LocationSendMessage
-from linebot.models import ButtonsTemplate
+from linebot.models import MessageEvent, TemplateSendMessage, MessageTemplateAction, TextSendMessage, TextMessage, PostbackEvent, PostbackTemplateAction, LocationSendMessage, ImagemapSendMessage
+from linebot.models import ButtonsTemplate, BaseSize
 from linebot.models import BubbleContainer, ImageComponent, BoxComponent, TextComponent
 from linebot.models import IconComponent, ButtonComponent, SeparatorComponent, DatetimePickerAction, PostbackAction
-from linebot.models import FlexSendMessage, URIAction, MessageAction
+from linebot.models import FlexSendMessage, URIAction, MessageAction, MessageImagemapAction, ImagemapArea
 from urllib.parse import parse_qsl
 
 
@@ -19,9 +19,72 @@ parser = WebhookParser(os.getenv("LINE_CHANNEL_SECRET"))
 
 def sendDepartmentTemplate(event):
   try:
+    image_url = 'https://imgur.com/czLUJcC.png'#商
+    imgwidth = 242
+    imgheight = 296
     message = [
       TextSendMessage(text="請點選科系："),
-      # TODO 把科系回復的樣板建立在這裡，用 message 的方式回覆ㄛ
+      ImagemapSendMessage(
+        base_url = image_url,
+        alt_text="選擇科系",
+        base_size=BaseSize(height = imgheight, width = imgwidth), #圖片寬、高
+        actions=[
+          MessageImagemapAction( #顯示文字訊息
+            text='會計學系',
+            area=ImagemapArea( #設定圖片範圍
+              x=0,
+              y=0,
+              width=imgwidth,
+              height=imgheight*0.25 
+            )
+          ),
+          MessageImagemapAction( #顯示文字訊息
+            text='財務金融學系',
+            area=ImagemapArea( #設定圖片範圍
+              x=0,
+              y=0,
+              width=imgwidth,
+              height=imgheight*0.5 
+            )
+          ),
+          MessageImagemapAction( #顯示文字訊息
+            text='國際經營與貿易學系',
+            area=ImagemapArea( #設定圖片範圍
+              x=0,
+              y=0,
+              width=imgwidth,
+              height=imgheight*0.75
+            )
+          ),
+          MessageImagemapAction( #顯示文字訊息
+            text='資訊管理學系',
+            area=ImagemapArea( #設定圖片範圍 #右邊出不來RRRRR
+              x=imgwidth*0.5,
+              y=imgheight*0.25,
+              width=imgwidth*0.5,
+              height=imgheight*0.25
+            )
+          ),
+          MessageImagemapAction( #顯示文字訊息
+            text='企業管理學系',
+            area=ImagemapArea( #設定圖片範圍
+              x=0,
+              y=0,
+              width=imgwidth,
+              height=imgheight
+            )
+          ),
+          MessageImagemapAction( #顯示文字訊息
+            text='國際商學學士學程',
+            area=ImagemapArea( #設定圖片範圍
+              x=0,
+              y=0,
+              width=imgwidth*0.5,
+              height=imgheight*0.25
+            )
+          ),
+        ]
+      )    
     ]
     line_bot_api.reply_message(event.reply_token, message)
   except Exception as e:
